a075ef1ebc87eafbd3bbf3b235d6084e6119376c
lakshya-bennett <lakshya.bennett@gmail.com>
2023-08-24 11:56:43 +0530

    msg_02

--- e22cseu0072.py
+++ e22cseu0072.py
@@ -1,56 +1,54 @@
 class FlightTable:
-    def __init__(self, locations, teams, timings):
+    def __init__(self):
         self.matches = []
-        self.locations = locations
-        self.teams = teams
-        self.timings = timings
 
     def add_match(self, location, team1, team2, timing):
-        self.matches.append((location, team1, team2, timing))
+        self.matches.append({
+            "Location": location,
+            "Team 01": team1,
+            "Team 02": team2,
+            "Timing": timing
+        })
 
     def search_by_team(self, team_name):
         team_matches = []
         for match in self.matches:
-            if team_name in match[1] or team_name in match[2]:
+            if team_name in [match["Team 01"], match["Team 02"]]:
                 team_matches.append(match)
         return team_matches
 
     def search_by_location(self, location_name):
         location_matches = []
         for match in self.matches:
-            if location_name in match[0]:
+            if location_name == match["Location"]:
                 location_matches.append(match)
         return location_matches
 
     def search_by_timing(self, timing_type):
         timing_matches = []
         for match in self.matches:
-            if timing_type in match[3]:
+            if timing_type == match["Timing"]:
                 timing_matches.append(match)
         return timing_matches
 
 def main():
-    locations = ["Mumbai", "Delhi", "Chennai", "Indore", "Mohali"]
-    teams = ["India", "England", "Australia", "Sri Lanka", "South Africa"]
-    timings = ["DAY", "DAY-NIGHT"]
-    
-    flight_table = FlightTable(locations, teams, timings)
+    flight_table = FlightTable()
     flight_table.add_match("Delhi", "India", "England", "DAY")
     flight_table.add_match("Mohali", "India", "Australia", "DAY-NIGHT")
     flight_table.add_match("Delhi", "India", "Australia", "DAY-NIGHT")
     flight_table.add_match("Chennai", "India", "England", "DAY")
     flight_table.add_match("Indore", "Australia", "South Africa", "DAY-NIGHT")
     flight_table.add_match("Mumbai", "Sri Lanka", "Australia", "DAY")
-    
+
     while True:
         print("Search Options:")
         print("1. List of all the matches of a Team")
         print("2. List of Matches on a Location")
         print("3. List of Matches based on timing")
         print("4. Exit")
-        
+
         choice = int(input("Enter your choice: "))
-        
+
         if choice == 1:
             team_name = input("Enter team name: ")
             team_matches = flight_table.search_by_team(team_name)
